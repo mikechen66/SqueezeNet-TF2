b36c385ac57c07ddc1e92ec55c7175c0d2a028dd
mikechen66 <875661606@qq.com>
2020-10-15 01:08:20 +0800

    Update senet_tf2.py

--- senet_tf2/senet_tf2.py
+++ senet_tf2/senet_tf2.py
@@ -2,30 +2,18 @@
 # coding: utf-8
 
 # senet_tf2.py
-
 """
-Please remember that it is the TensorFlow realization with image_data_foramt = 'channels_last'. If
-the env of Keras is 'channels_first', please change it  according to the TensorFlow convention.  
-Grouped Convolution Layer is implemented as a Slice, Conv2D and Concatenate Split filters to groups. 
+It is the general SqueezeNet realizations that cover the major model variants. Grouped Convolution Layer 
+is implemented as a Slice, Conv2D and Concatenate Split filters to groups. 
 
-# Set up the GPU memory size to avoid the out-of-memory error if the GPU setting has a problem 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-  # Restrict TensorFlow to only allocate 4GB of memory on the first GPU
-  try:
-    tf.config.experimental.set_virtual_device_configuration(
-        gpus[0],
-        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
-    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-  except RuntimeError as e:
-    # Virtual devices must be set before GPUs have been initialized
-    print(e)
+Please remember that it is the TensorFlow realization with image_data_foramt = 'channels_last'. If the env
+of Keras is 'channels_first', please change it according to the TensorFlow convention.  
 
 Make the necessary changes to adapt to the environment of TensorFlow 2.3, Keras 2.4.3, CUDA Toolkit 11.0, 
-cuDNN 8.0.1 and CUDA 450.57. In addition, write the new lines of code to replace the deprecated code. The 
-script has many modifications on the foundation of is ResNet50 by Francios Chollet, qubvel and many other 
-published results. I would like to thank all of them for the contributions. 
+cuDNN 8.0.1 and CUDA 450.57. In addition, write the new lines of code to replace the deprecated code. 
+
+The script has many modifications on the foundation of is ResNet50 by Francios Chollet, qubvel and many 
+other published results. I would like to thank all of them for the contributions. 
 
 Environment: 
 Ubuntu 18.04 
